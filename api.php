466a7547f2a6d0571c3349d412a7dcc76ce3a178
Mashape <support@mashape.com>
2010-11-22 17:45:37 +0100

    fixed api.php and api.xml

--- api.php
+++ api.php
@@ -2,54 +2,16 @@
 
 require_once("mashape/mashape.php");
 
-class Ret {
-	public $name;
-	public $age;
-	public $nick;
-}
-
-class User {
-	public $name;
-	public $email;
-	public $ret;
-}
-
-class Tag {
-	public $text;
-	public $author;
-}
-
-class Second {
-	public $tag;
-	public $rating;
-	public $user;
-}
-
-class HelloWorldAPI extends MashapeRestAPI
+class YourAPI extends MashapeRestAPI
 {
-	public function sayHello($name, $nick = "default nickname") {
-		$ret = new Ret();
-		$ret->name = $name;
-		$ret->age = array(3, 4, 10);
-		$ret->nick = $nick;
-		return $ret;
+        // Your functions here
+	public function sayHello($name) {
+		return "Hello " . $name . "!";
 	}
-	
-	public function complex1($param1, $param2, $param3) {
-		return new User();
-	}
-	
-	public function complex2($param1) {
-		return new Second();
-	}
-	
-	public function touch() {
-		return "Ouch!";
-	}
+
 }
 
 // Handle the requests
-MashapeHandler::handleAPI(new HelloWorldAPI());
+MashapeHandler::handleApi(new YourAPI());
 
 ?>
-
